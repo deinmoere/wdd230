eb4372109d43a2cf95d0f5416cf52e5f4c8daed2
deinmoere <deinmo.ha@gmail.com>
2023-06-19 22:57:18 +0000

    Home page update

---
+++ chamber/scripts/getDate.js
@@ -0,0 +1,9 @@
+// declare and instantiate a variable to hold the current year
+const currentYear = 2023;
+
+// place the value of the current year variable into the HTML file
+document.querySelector('#currentYear').innerHTML = currentYear;
+
+let currentdate = document.lastModified;
+        // document.getElementById('modified').textContent = 'Last Updated: '+ currentdate + '.';
+document.getElementById('lastModified').textContent = `Last Updated ${currentdate} . `;
