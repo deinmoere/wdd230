89d5c3a377993cdfab9c177fd0981958d9636430
deinmoere <deinmo.ha@gmail.com>
2023-06-21 19:02:31 +0000

    Chamber home page

--- chamber/scripts/getDate.js
+++ chamber/scripts/getDate.js
@@ -7,3 +7,27 @@ document.querySelector('#currentYear').innerHTML = currentYear;
 let currentdate = document.lastModified;
         // document.getElementById('modified').textContent = 'Last Updated: '+ currentdate + '.';
 document.getElementById('lastModified').textContent = `Last Updated ${currentdate} . `;
+
+const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('.navigation');
+
+
+hamButton.addEventListener('click', () => {
+	navigation.classList.toggle('open');
+	hamButton.classList.toggle('open');
+});
+
+const modeButton = document.querySelector("#mode");
+const main = document.querySelector("main");
+
+modeButton.addEventListener("click", () => {
+	if (modeButton.textContent.includes("☑️")) {
+		main.style.background = "#000";
+		main.style.color = "#fff";
+		modeButton.textContent = "❎";
+	} else {
+		main.style.background = "#eee";
+		main.style.color = "#000";
+		modeButton.textContent = "☑️";
+	}
+});
