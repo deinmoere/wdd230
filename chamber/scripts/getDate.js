5df634134793aac0afa5c403a81885fd4cbb7bf5
deinmoere <deinmo.ha@gmail.com>
2023-07-01 02:44:58 +0000

    Discover page

--- chamber/scripts/getDate.js
+++ chamber/scripts/getDate.js
@@ -31,3 +31,55 @@ modeButton.addEventListener("click", () => {
 		modeButton.textContent = "☑️";
 	}
 });
+
+// Script for Number of Visits
+//let numVisits = document.querySelector('.daysOfVisit');
+
+//Script for displaying the number of days on Discover page
+
+/* let numOfVisits = Number(window.localStorage.getItem("visits"));
+let lastVisit= Number(window.localStorage.getItem("lastVisits"));
+
+const FACTOR = 1000 * 60 * 60 * 24;
+
+let daysBetween = Date.now() - lastVisit;
+
+let numOfDays = Math.ceil(daysBetween / FACTOR);
+
+localStorage.setItem("lastVisits", Date.now());
+
+if (numOfVisits !== 0) {
+	numVisits.textContent = "Welcome! Let us know if you have any questions."    
+} 
+
+else {
+	numVisits.textContent = "You last visited"  + numOfDays + "days ago."    
+}
+ */
+/* numOfVisits++;
+// store the new number of visits value
+localStorage.setItem("visits", numOfVisits); */
+
+// Days since last visit
+
+let todays = Date.now();
+
+// initialize display elements
+const visitsDisplay = document.querySelector(".daysOfvisits");
+
+// get the stored value in localStorage
+let lastVisit = Number(window.localStorage.getItem("visits-ls"));
+
+// determine if this is the first visit or display the number of visits.
+if (lastVisit > 0) {
+    let visitGap = Math.abs(todays - lastVisit) / 1000;
+    days = Math.floor(visitGap / 86400);
+    visitsDisplay.textContent = "You last visited"  + days + "days ago.";
+} else {
+    visitsDisplay.textContent = "Welcome! Let us know if you have any questions.";
+}
+
+// store the new number of visits value
+localStorage.setItem("visits-ls", todays);
+
+
